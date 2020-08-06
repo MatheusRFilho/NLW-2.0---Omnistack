c3214bd58ef59e6ee44b055b1cb2aca93304f271
MatheusRFilho <matheusrodrigofilho@outlook.com>
2020-08-06 18:56:59 -0300

    Fim dia 3

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,32 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    name: string;
+    avatar: string;
+    whatsapp: string;
+    bio: string;
+    subject: string;
+    cost: number;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars0.githubusercontent.com/u/61762071?s=460&v=4" alt="Avatar"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Matheus Rodrigo</strong>
-                            <span>Química</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
 
                     <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br /><br />
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências.
+                        {teacher.bio}
                     </p>
                     <footer>
                         <p>
                             Preço/hora:
-                            <strong>R$ 80,00</strong>
+                            <strong>R$ {teacher.cost}</strong>
                         </p>
-                        <button type="button">
+                        <a 
+                            target="_blank" 
+                            onClick={createNewConnection}  
+                            href={`https://wa.me/${teacher.whatsapp}`}
+                            rel="noopener noreferrer">
                             <img src={whatsappIcon} alt="whatsapp" />
                             Entrar em contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     );
